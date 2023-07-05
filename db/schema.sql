1e133b39526d37d80bc271f9c346065bba32c2b8
myra-k <myrakhatoon90@gmail.com>
2023-07-05 17:51:33 +0100

    fixed error in schema with employee table

--- db/schema.sql
+++ db/schema.sql
@@ -23,11 +23,11 @@ CREATE TABLE role
 CREATE TABLE employee
 (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    first_name VARCHAR(30),
-    last_name VARCHAR(30),
+    first_name VARCHAR (30),
+    last_name VARCHAR (30),
     role_id INT,
     manager_id INT,
-    FOREIGN KEY (department_id)
-    REFERENCES department(id)
+    FOREIGN KEY (role_id)
+    REFERENCES role(id)
     ON DELETE SET NULL
 );
